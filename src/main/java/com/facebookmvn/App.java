b3edaa40830e86d08c218c8e4ece41b92f08cdad
narenpuppala <naren26@WebAppFacebooklive.com>
2022-08-23 06:42:07 +0000

    add maven project

---
+++ src/main/java/com/facebookmvn/App.java
@@ -0,0 +1,13 @@
+package com.facebookmvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {//i am trying to push this entire Project to push to git repository...
+        System.out.println( "Hello World!" );
+    }
+}
